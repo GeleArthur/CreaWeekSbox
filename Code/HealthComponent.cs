60234bfd953077504a51a35221f13b44ea4d65d1
DbroKing <62269519+David-Devos@users.noreply.github.com>
2025-04-01 19:49:21 +0200

    Zombie has Health and can ragdoll (cannot take damage yet)

--- Code/HealthComponent.cs
+++ Code/HealthComponent.cs
@@ -3,10 +3,11 @@ using Sandbox;
 
 public sealed class HealthComponent : Component
 {
-	private int _health;
+	[Property]
 	private int _maxHealth;
 
 	public Action OnDeath;
+	private int _health;
 
 	public int Health
 	{
@@ -14,17 +15,18 @@ public sealed class HealthComponent : Component
 		private set
 		{
 			_health = value;
-			if ( _health == 0 ) OnDeath?.Invoke();
+			if ( _health <= 0 ) OnDeath?.Invoke();
 		}
 	}
 
 	public void Damage( int damage )
 	{
-		_health = int.Clamp( _health - damage, 0, _maxHealth );
+		Health = int.Clamp( _health - damage, 0, _maxHealth );
 	}
 
 	public void Heal( int healing )
 	{
-		_health = int.Clamp( _health + healing, 0, _maxHealth );
+		Health = int.Clamp( _health + healing, 0, _maxHealth );
 	}
+
 }
