a416195e6192387854668c12b127f4bf18adbee9
DbroKing <62269519+David-Devos@users.noreply.github.com>
2025-04-01 17:03:10 +0200

    Chase became movecomp (wander and chase combined)

---
+++ Code/ZombieMoveComp.cs
@@ -0,0 +1,79 @@
+using Sandbox;
+using System;
+
+public sealed class ZombieMoveComp : Component
+{
+	[Property]
+	public string Name { get; set; }
+
+	[Property]
+	private GameObject target;
+	[Property]
+	private float chaseRadius;
+	[Property]
+	private float alertRadius;
+	[Property]
+	private float wanderSize;
+	private SphereCollider chaseRange;
+	private SphereCollider alertRange;
+	private BoxCollider wanderRange;
+	private NavMeshAgent agent;
+	private bool isChasing = false;
+	private float wanderTimer = 0;
+	private float wanderMaxTime = 5;
+	private float wanderTimeOffset = 2;
+	private Random random = new Random();
+	protected override void OnStart()
+	{
+		chaseRange = GameObject.AddComponent<SphereCollider>();
+		chaseRange.Radius = chaseRadius;
+		chaseRange.IsTrigger = true;
+		chaseRange.OnTriggerExit += ( other ) =>
+		{
+			if ( other.GameObject.Parent.Name == "Player Controller" )
+			{
+				isChasing = false;
+			}
+		};
+		alertRange = GameObject.AddComponent<SphereCollider>();
+		alertRange.Radius = alertRadius;
+		alertRange.IsTrigger = true;
+		alertRange.OnTriggerEnter += ( other ) =>
+		{
+			if ( other.GameObject.Parent.Name == "Player Controller" )
+			{
+				isChasing = true;
+			}
+		};
+		wanderRange = GameObject.AddComponent<BoxCollider>();
+		wanderRange.Scale =  new Vector3(  wanderSize, wanderSize, 20 );
+		wanderRange.LocalTransform = LocalTransform;
+		wanderRange.IsTrigger = true;
+		agent = GameObject.GetComponent<NavMeshAgent>();
+	}
+	protected override void OnUpdate()
+	{
+		if ( isChasing )
+		{
+			agent.MoveTo( target.WorldPosition );
+		}
+		else
+		{
+			Wander();
+		}
+	}
+	private void Wander()
+	{
+		if ( wanderTimer <= 0 )
+		{
+			Vector3 randomPos = wanderRange.LocalBounds.RandomPointInside;
+			randomPos = (Vector3)Scene.NavMesh.GetClosestPoint( randomPos );
+			agent.MoveTo( LocalPosition + randomPos );
+			wanderTimer = wanderMaxTime + random.Float( -wanderTimeOffset, wanderTimeOffset );
+		}
+		else
+		{
+			wanderTimer -= Time.Delta;
+		}
+	}
+}
