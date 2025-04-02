c4d0edf9a710f0347af067352705c24b6a7b081e
GeleArthur <geleminecrafter@gmail.com>
2025-04-02 02:37:59 +0200

    Better zombie ragdoll

--- Code/OnCollideRagdoll.cs
+++ Code/OnCollideRagdoll.cs
@@ -2,9 +2,31 @@ using Sandbox;
 
 public sealed class OnCollideRagDoll : Component, Component.ICollisionListener
 {
+	private ModelPhysics _ragdollPhysics;
+	private Rigidbody _rigidbody;
+	private ModelCollider _modelCollider;
+	
+	protected override void OnStart()
+	{
+		_ragdollPhysics =  GetComponent<ModelPhysics>();
+		_rigidbody = GetComponent<Rigidbody>();
+		_modelCollider = GetComponent<ModelCollider>();
+		_ragdollPhysics.Enabled = false;
+		_rigidbody.Enabled = true;
+		_modelCollider.Enabled = true;
+
+	}
+
 	public void OnCollisionStart( Collision collision )
 	{
-		Log.Info( collision.Other.GameObject.Name );
-		GetComponent<ModelPhysics>().MotionEnabled = true;
+		// if(!collision.Other.GameObject.Tags.Has("car")) return;
+		// Log.Info( collision.Contact.Speed.Length );
+		
+		if(collision.Contact.Speed.Length < 250) return;
+		
+		_ragdollPhysics.Enabled = true;
+		_rigidbody.Enabled = false;
+		_modelCollider.Enabled = false;
+
 	}
 }
