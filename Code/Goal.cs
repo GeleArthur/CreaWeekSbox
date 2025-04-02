1e5c642afdfd84511e61963e3903e516991ca553
EliasStoffels <Elias.Stoffels5752005@gmail.com>
2025-04-02 11:38:52 +0200

    Goals are now visually different and added a text renderer to see which is which

--- Code/Goal.cs
+++ Code/Goal.cs
@@ -1,11 +1,14 @@
-using Sandbox;
+﻿using Sandbox;
+using System;
 
 public sealed class Goal : Component
 {
 	[Property]
-	public ModelRenderer Model;
+	public ModelRenderer Model {  get; set; }
 	[Property]
-	public bool SellingGoal;
+	public bool SellingGoal { get; set; }
+	[Property]
+	public TextRenderer TextRendererComp { get; set; }
 
 	private Collider _collider;
 	public Collider Collider
@@ -17,26 +20,68 @@ public sealed class Goal : Component
 	private GoalManager _goalManager;
 	public GoalManager GoalManager
 	{
-		get { return _goalManager; }
+		private get { return _goalManager; }
 		set { _goalManager = value; }
 	}
 
+	private GameObject _player;
+
 	protected override void OnStart()
 	{
+		_player = Scene.Directory.FindByName( "Player Controller" ).First();
+
 		Collider = GetComponent<Collider>();
 		Collider.OnTriggerEnter = ( Collider other ) =>
 		{
 			if ( other.Tags.Has("player") ||other.Tags.Has("car"))
 			{
+				if(GoalManager == null)
+				{
+					GoalManager = Scene.Directory.FindByName( "GoalManager" ).First().GetComponent<GoalManager>();
+				}
 				GoalManager.Notify( this.GameObject , SellingGoal);
 			}
 		};
+		if ( SellingGoal )
+		{ 
+			Model.Tint = new Color( 0f, 1f, 0f, 0.4f );
+			TextRendererComp.Text = "🤑";
+		}
+		else
+		{
+			Model.Tint = new Color( 0f, 0f, 1f, 0.4f );
+			TextRendererComp.Text = "💊";
+		}
+			
+	}
 
-		Log.Info(SellingGoal);
+	protected override void OnUpdate()
+	{
+		if ( !TextRendererComp.IsValid )
+		{
+			Log.Info( "rip" );
+			return;
+		}
+
+		if (TextRendererComp.Active)
+		{
+			var direction = _player.WorldPosition - WorldPosition;
+			direction = direction / direction.Length;
+			var angle = Math.Atan2( direction.y, direction.x );
+			angle = angle / double.Pi * 180;
+
+			TextRendererComp.LocalTransform = new Transform( TextRendererComp.LocalPosition, new Angles( 0, (float)angle, 0 ) );
+		}
 	}
 
 	public void EnableModel(bool enable)
 	{
 		Model.Enabled = enable;
+		if ( TextRendererComp == null )
+		{
+			Log.Info( "also here" );
+			return;
+		}
+		TextRendererComp.Enabled = enable;
 	}
 }
